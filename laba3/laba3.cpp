ffdb89cdaf1db0bbfb6b064cd6c7af614ef08abb
NEGAvv <hudolija@gmail.com>
2021-10-21 19:13:34 +0300

    Добавьте файлы проекта.

---
+++ laba3/laba3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include "windows.h"
+#include <iomanip>
+#include "math.h"
+using namespace std;
+int main()
+{
+	float minX = 1.5, maxX = 2.5, h = 0.1, x;
+	double y;
+	cout << "Input minX=1.5" << endl;
+	cout << "Input maxX=2.5" << endl;
+	cout << "Input h=0.1" << endl;
+	x = minX;
+	do
+	{
+		y = (x * x * x) + 3 * sin(x * x) - 3;
+		cout << setw(4) << x << setw(10) << y << endl;
+		x += h;
+	} while (x <= maxX + h / 2);
+}
